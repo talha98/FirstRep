80f5ed1b8d2cb51187859785dc75dfe7c16b673c
Talha Zubair <talha.ansari6212@gmail.com>
2021-07-05 17:01:13 +0530

    InitialApp

---
+++ MyFirstApp/MyFirstConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            
+            ClassLibrary.Class1 sc = new ClassLibrary.Class1();
+            string outp = sc.CallMe(" Talha");
+            Console.WriteLine(outp);
+            Console.ReadLine();
+        }
+    }
+}
